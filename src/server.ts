8cf8110f2f335d36663c71a5f50db77e266d0110
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-10-10 21:15:02 -0300

    primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
+
+const server = express();
+
+server.use(express.json());
+
+server.use(categoriesRoutes);
+
+server.listen(3333);
