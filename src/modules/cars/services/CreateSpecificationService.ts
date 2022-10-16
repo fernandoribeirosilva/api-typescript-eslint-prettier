d628a446f4c6bb30906c1250668e8db7ed19b9ec
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-10-16 09:22:19 -0300

    criado metodo de specifications

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,8 +1,20 @@
+import { SpecificationsRepository } from "../repositories/SpecificationsRepository";
 
+interface IRequest {
+  name: string;
+  description: string;
+}
 
 class CreateSpecificationService {
-  exeicute() {
-    console.log("TODO");
+  constructor(private specificationsRepository: SpecificationsRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+    if (specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists`);
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
