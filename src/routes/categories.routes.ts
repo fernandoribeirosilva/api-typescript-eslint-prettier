7d04a77c6da20d0dc08aef978768c3c07e34002b
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-12-12 10:17:55 -0300

    criando use case de especificação

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,8 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = CategoriesRepository.getInstance();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
