a4fab174ffef5ef78ec8a5d6996341dcb662327e
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-12-19 15:55:09 -0300

    usando biblioteca csv-parse

--- src/modules/cars/useCases/importCategory/ImportCatergoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCatergoryUseCase.ts
@@ -1,6 +1,19 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "node:fs";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File | null | undefined): void {
+    if (!file) {
+      throw new Error("File not found!");
+    }
+
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
