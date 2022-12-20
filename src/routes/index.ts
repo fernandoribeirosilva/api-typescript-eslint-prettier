44b78ee7620fbeda3037843bebcd64f9756a43a3
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-12-20 15:02:52 -0300

    fazendo a documentação da aplicação com o swagger é adicionado uma rota de ping pong

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 
 const router = Router();
+
+router.get("/ping", (req: Request, res: Response) => {
+  return res.status(200).json({ pong: true });
+});
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 server.use(router);
 
 server.listen(3333);
